c490124331bae4108402c539524866563cd5139d
Judymwelu/alx-low_level_programming <judymwelu00@gmail.com>
2023-07-13 15:21:39 -0700

    task3

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -13,4 +13,5 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _strlen(char *s);
 int is_digit(char *s);
+void errrors(void);
 #endif
