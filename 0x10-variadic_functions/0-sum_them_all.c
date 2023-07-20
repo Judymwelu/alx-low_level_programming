be078d1bb7ce42c8f89bd938df9eb3f685cda281
Judymwelu/alx-low_level_programming <judymwelu00@gmail.com>
2023-07-20 15:05:02 -0700

    task0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_function.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - sumsthe pararmeters
+ * @n: number of parameters
+ * @...: a variable number of parameters
+ * Return: sum of parameters else 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum = 0;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+	return (sum);
+}
